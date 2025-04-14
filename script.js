f5960c4d30d5ca69066695281916959146b0482c
Sourabh Patnaik <sourabhpatnayak19@gmail.com>
2025-04-14 17:32:00 +0530

    Updated the whole Approach

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(title, author, genre, page, status) {
+  this.title = title;
+  this.author = author;
+  this.genre = genre;
+  this.page = page;
+  this.status = status;
+}
+
+function addBookToLibrary() {
+
+}
